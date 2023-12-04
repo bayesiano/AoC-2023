ae447fecf58e7bb4907ea5dae3002ec22b2a3145
adiazgi <antoniodiaz@ibermutua.es>
2023-12-04 09:12:17 +0100

    Day04

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,63 @@
+import java.util.*
+import kotlin.math.min
+
+object Day04 {
+
+    data class Card( val cardNumber: Int, val winningNumbers: Set<Int>, val cardNumbers: Set<Int>) {
+        val wins = winningNumbers.intersect(cardNumbers).size
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val example1 = runProblem("day04/example_1.txt", ::solveProblem1)
+        assert(example1 == 13)
+
+        val problem1 = runProblem("day04/problem_1.txt", ::solveProblem1)
+        assert(problem1 == 21485)
+
+        val example2 = runProblem("day04/example_1.txt", ::solveProblem2)
+        assert(example2 == 30)
+
+        val problem2= runProblem("day04/problem_1.txt", ::solveProblem2)
+        assert(problem2 == 11024379)
+    }
+
+    private fun solveProblem1(lines: List<String>): Int {
+        val cards = readCards(lines)
+        return cards.map { powerOf(it.wins) }.sum()
+    }
+
+    private fun solveProblem2(lines: List<String>): Int {
+        val cards = readCards(lines)
+
+        val cardCopies = Array<Int>( cards.size) {1}
+        cards.forEachIndexed { i, card ->
+            (i + 1..min(i+card.wins,cardCopies.size-1)).forEach { j ->
+                cardCopies[j] += cardCopies[i]
+            }
+        }
+        return cardCopies.sum()
+    }
+
+    private fun readCards(lines: List<String>) =
+        lines.map { line ->
+            val scanner  = Scanner(line).useDelimiter("[ :]+")
+            scanner.next() // Card
+            val cardNumber = scanner.nextInt()
+            val winningNumbers = mutableSetOf<Int>()
+            while( scanner.hasNextInt()) winningNumbers += scanner.nextInt()
+            scanner.next() // |
+            val cardNumbers = mutableSetOf<Int>()
+            while( scanner.hasNextInt()) cardNumbers += scanner.nextInt()
+
+            Card( cardNumber, winningNumbers, cardNumbers)
+        }
+
+    private fun powerOf( exp: Int): Int {
+        if( exp == 0) return 0
+        var res = 1
+        (2..exp).forEach { res *= 2 }
+        return res
+    }
+
+}
